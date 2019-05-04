fa751d41ef05b26367df51ffea2d3d88330d96dc
klrs <kllrssn@gmail.com>
2019-05-04 12:18:57 +0300

    kalenteri takaisin jenkkimuotoon, wip

--- js/calendar_scripts.js
+++ js/calendar_scripts.js
@@ -1,53 +1,46 @@
 //10x8
 
-function init() {
+function calendar() {
     //NEEDS TESTING
-    const days = ['unused','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday','Sunday'];
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
     let date = new Date();
-    var currentDay = date.getDay() || 7 ;
+    var currentDay = date.getDay();
     var weekdays = document.getElementsByClassName("weekdays");
-    var oldDay = currentDay;
-    var newDay = currentDay;
 
-    setCurrentDayColor(currentDay);
-    setCellCallbacks();
-    setTblHeaders(date,currentDay);
+    setup();
 
-    function setCurrentDayColor(currentDay) {
-        //colors current day red
-        newDay = currentDay;
-        weekdays[newDay - 1].style.backgroundColor = "red";
-
-        if(newDay != oldDay){
-            weekdays[oldDay - 1].style.backgroundColor = "#31353a";
-        }
-        oldDay = newDay;
-    }
-
-    document.getElementById("navi").onchange = function() {
+    document.getElementById("navisubm").onclick = function() {
         //datepicker/calendar navigator
-        let date = document.getElementById('navi').valueAsDate;
-        let newCurDay = date.getDay() || 7 ;
+        date = document.getElementById('navi').valueAsDate;
+        setup();
+
         //new dates to headers
-        setTblHeaders(date,newCurDay);
-        setCurrentDayColor(newCurDay);
-    }
+        //setTblHeaders(date,newCurDay);
+        //setCurrentDayColor(newCurDay);
+    };
 
-    function setTblHeaders(date,currentDay) {
-        //generates current week dates to table headings
-        document.getElementById('navi').value = date.toISOString().slice(0, 10);
-       // alert("headertest."+ date); test alert
-
-        date.setDate(date.getDate() - currentDay + 1);
-
-        for(var i = 0; i < 7; i++) {
-            weekdays[i].innerHTML = days[date.getDay() || 7] + " " + (date.getDate()) + "." + (date.getMonth()+1) + "." + date.getFullYear();
-            //weekdays[i].innerHTML = days[date.getDay() || 7] + " " + date.toISOString().slice(0, 10);
-            date.setDate(date.getDate() + 1);
+    function setCurrentDayColor() {
+        //colors current day red
+        for(let i = 0; i < weekdays.length; i++) {
+            if(currentDay === i) {
+                weekdays[i].style.backgroundColor = "red";
+            }
+            else {
+                weekdays[i].style.backgroundColor = "transparent";
+            }
         }
     }
-
+    function setTblHeaders() {
+        //generates current week dates to table headings
+        let newdate = date;
+        newdate.setDate(newdate.getDate() - currentDay);
+        for(var i = 0; i < 7; i++) {
+            //weekdays[i].innerHTML = days[date.getDay()] + " " + (date.getDate()+1) + "." + (date.getMonth()+1) + "." + date.getFullYear();
+            weekdays[i].innerHTML = days[newdate.getDay()] + " " + newdate.toISOString().slice(0, 10);
+            newdate.setDate(newdate.getDate() + 1);
+        }
+    }
     function setCellCallbacks() {
         //assigns callback function to each cell
         let td = document.getElementsByTagName("td");
@@ -56,7 +49,7 @@ function init() {
         for(let time = 0; time < 9; time++) {
             for(let day = 0; day < 7; day++) {
                 let c = time * 7 + day;
-                cells.push(new Cell(day + 1, time));
+                cells.push(new Cell(day, time));
                 td[c].onclick = function() {
                     form_date.elements["date"].value = cells[c].day;
                     form_date.elements["start_time"].value = cells[c].time;
@@ -65,10 +58,16 @@ function init() {
             }
         }
         function Cell(day, time) {
-            let date = new Date();
-            date.setDate((date.getDate() - (currentDay)) + day);
+            let newdate = date;
+            newdate.setDate((newdate.getDate() - (currentDay)) + day);
             this.time = (14 + time) + ':' + '00';
-            this.day = date.toISOString().slice(0, 10);
+            this.day = newdate.toISOString().slice(0, 10);
         }
+    }
+    function setup() {
+        currentDay = date.getDay();
+        setCurrentDayColor();
+        setTblHeaders();
+        setCellCallbacks();
     }
 }
