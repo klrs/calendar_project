2fe2c7303a7986ea2d79e7084cedffa42eb3e20a
Lappari <Joonas.Lapinlampi@gmail.com>
2019-05-06 14:55:23 +0300

    Merge Lapparin koodin kanssa valmis.

--- js/calendar_scripts.js
+++ js/calendar_scripts.js
@@ -2,44 +2,48 @@
 
 function calendar() {
     //NEEDS TESTING
-    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const days = ['unused','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday','Sunday'];
 
-    let date = new Date();
-    var currentDay = date.getDay();
+    var date = new Date();
+    var currentDay = date.getDay() || 7 ;
     var weekdays = document.getElementsByClassName("weekdays");
-    let cells = []; //cells will get initialized in setCellCallbacks()
+    let cells = [];
+    var oldDay = currentDay;
+    var newDay = currentDay;
 
     setup();
 
-    document.getElementById("navisubm").onclick = function() {
+    document.getElementById("navi").onchange = function() {calendarNav()};
+
+    function calendarNav() {
         //datepicker/calendar navigator
         date = document.getElementById('navi').valueAsDate;
         setup();
-
-        //new dates to headers
-        //setTblHeaders(date,newCurDay);
-        //setCurrentDayColor(newCurDay);
-    };
+    }
 
     function setCurrentDayColor() {
         //colors current day red
-        for(let i = 0; i < weekdays.length; i++) {
-            if(currentDay === i) {
-                weekdays[i].style.backgroundColor = "red";
-            }
-            else {
-                weekdays[i].style.backgroundColor = "transparent";
-            }
+        newDay = currentDay;
+        //header color from bootstrap class
+        weekdays[newDay - 1].classList.add("bg-danger");
+        if(newDay != oldDay){
+            //remove class from old date
+            weekdays[oldDay - 1].classList.remove("bg-danger");
         }
+        oldDay = newDay;
     }
     function setTblHeaders() {
+        //generate current week number to week heading
+        document.getElementById("currentWeek").innerHTML = "Week " + getWeekOfYear();
+        //generate date to calendarNav ISOString timezone fix
+        var isoDate = new Date(date.getTime() - (date.getTimezoneOffset() * 60000));
+        document.getElementById('navi').value = isoDate.toISOString().slice(0 , 10);
         //generates current week dates to table headings
         let newdate = new Date(+date);  //make a new date object so the original won't get muddled
-
-        newdate.setDate(newdate.getDate() - currentDay);
+        newdate.setDate(newdate.getDate() - currentDay + 1);
         for(var i = 0; i < 7; i++) {
-            //weekdays[i].innerHTML = days[date.getDay()] + " " + (date.getDate()+1) + "." + (date.getMonth()+1) + "." + date.getFullYear();
-            weekdays[i].innerHTML = days[newdate.getDay()] + " " + newdate.toISOString().slice(0, 10);
+            weekdays[i].innerHTML = days[newdate.getDay() || 7] + " " + (newdate.getDate()) + "." + (newdate.getMonth()+1) + "." + newdate.getFullYear();
+           // weekdays[i].innerHTML = days[newdate.getDay()] + " " + newdate.toISOString().slice(0, 10);
             newdate.setDate(newdate.getDate() + 1);
         }
     }
@@ -51,11 +55,13 @@ function calendar() {
         }
         function Cell(day, time) {
             let newdate = new Date(+date);
-            newdate.setDate((newdate.getDate() - (currentDay)) + day);
+            newdate.setDate((newdate.getDate() - (currentDay)) + day + 1);
 
-            this.time = (14 + time) + ':' + '00';
+            this.time = (14 + time) + ':' + '00 - ' + (15+time) + ':' + '00';
             this.index_time = time;
-            this.day = newdate.toISOString().slice(0, 10);
+            //isoDate timezone fix
+            var isoDate = new Date(newdate.getTime() - (newdate.getTimezoneOffset() * 60000));
+            this.day = isoDate.toISOString().slice(0, 10);
             this.index_day = day;
             this.reserved = false;
         }
@@ -85,8 +91,40 @@ function calendar() {
         cells[0].reserved = true;
 
     }
+    function getWeekOfYear() {
+        var target = new Date(date.valueOf()),
+            dayNumber = (date.getDay() + 6) % 7,
+            firstThursday;
+
+        target.setDate(target.getDate() - dayNumber + 3);
+        firstThursday = target.valueOf();
+        target.setMonth(0, 1);
+
+        if (target.getDay() !== 4) {
+            target.setMonth(0, 1 + ((4 - target.getDay()) + 7) % 7);
+        }
+
+        return Math.ceil((firstThursday - target) /  (7 * 24 * 3600 * 1000)) + 1;
+    }
+
+    //nav arrow backwards
+    document.getElementById("weekArrowBack").onclick = function () {
+        let dateArrow = document.getElementById('navi').valueAsDate;
+        dateArrow.setDate(dateArrow.getDate() - 7);
+        date = new Date(+dateArrow);
+        setup();
+    }
+
+    //nav arrow forwards
+    document.getElementById("weekArrowForward").onclick = function () {
+        let dateArrow = document.getElementById('navi').valueAsDate;
+        dateArrow.setDate(dateArrow.getDate() + 7);
+        date = new Date(+dateArrow);
+        setup();
+    }
+
     function setup() {
-        currentDay = date.getDay();
+        currentDay = date.getDay() || 7;
         cells.length = 0;
         initCells();
         setCurrentDayColor();
